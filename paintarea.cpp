13f3e9cd7f611d7b26f85fecc0a7b45cbe7f7568
sgsdongfang <2976904643@qq.com>
2023-08-30 16:21:21 +0800

    加了覆盖顺序

--- paintarea.cpp
+++ paintarea.cpp
@@ -36,58 +36,58 @@ void paintarea::paintEvent(QPaintEvent *event)
     // 绘图
     QPainter painter(this);
     pixfly.load(":/qss/fly_ui.png");
-//    painter.drawPixmap(50, 750, 50, 50, pixfly);
-//    painter.drawPixmap(100, 750, 50, 50, pixfly);
-//    painter.drawPixmap(150, 750, 50, 50, pixfly);
     pixcar.load(":/qss/car.png");
-//    painter.drawPixmap(150, 50, 80, 50, pixcar);
-//    painter.drawPixmap(650, 50, 80, 50, pixcar);
+    int width[3]={40,40,40};
+    int height[3]={40,40,40};
+    int horder[3]={1,2,3};
 
-//    float x = -idy[0]/10;
-//    float y = -idx[0]/10;
-
-    // 利用更改坐标原点实现平移
-    int width[5]={40,40,40,40,40};
-    int height[5]={40,40,40,40,40};
     for(int i=0; i<3; i++)
     {
-        qDebug()<< "idz:" << idz[i];
         width[i] = width[i]/(1 + (float)idz[i]*0.5/200);
         height[i] = height[i]/(1 + (float)idz[i]*0.5/200);
+//        for(int j=i;j>0;j--)//高度排序
+//        {
+//            if(idz[horder[j]]<idz[horder[j-1]]){
+//                int temp=horder[j-1];
+//                horder[j-1]=horder[j];
+//                horder[j]=temp;
+//            }
+//        }
     }
-    // qDebug() << this->width() << " " << this->height();
     painter.translate(this->width()/2, this->height()/2);//将（100，100）设为坐标原点
 
 
     painter.translate(idx[3],idy[3]);
-    painter.rotate(idw[3]); //旋转90
+    painter.rotate(idw[3]); //旋转
     painter.drawPixmap(0,0,40,30,pixcar);  //小车4
     painter.rotate(-idw[3]); //旋转
     painter.translate(-idx[3],-idy[3]);
 
     painter.translate(idx[4],idy[4]);
-    painter.rotate(idw[4]); //旋转90
+    painter.rotate(idw[4]); //旋转
     painter.drawPixmap(0,0,40,30,pixcar);  //小车5
     painter.rotate(-idw[4]); //旋转
     painter.translate(-idx[4],-idy[4]);
 
-    painter.translate(idx[0],idy[0]);
-    painter.rotate(idw[0]); //旋转
-    painter.drawPixmap(0,0,width[0],height[0],pixfly);  //飞机1
-    painter.rotate(-idw[0]); //旋转
-    painter.translate(-idx[0],-idy[0]);
+    for(int i=0;i<3;i++)
+    {
+        painter.translate(idx[horder[i]],idy[horder[i]]);
+        painter.rotate(idw[horder[i]]); //旋转
+        painter.drawPixmap(0,0,width[horder[i]],height[horder[i]],pixfly);  //飞机
+        painter.rotate(-idw[horder[i]]); //旋转
+        painter.translate(-idx[horder[i]],-idy[horder[i]]);
+    }
+//    painter.translate(idx[1],idy[1]);
+//    painter.rotate(idw[1]); //旋转
+//    painter.drawPixmap(0,0,width[1],height[1],pixfly);  //飞机2
+//    painter.rotate(-idw[1]); //旋转
+//    painter.translate(-idx[1],-idy[1]);
 
-    painter.translate(idx[1],idy[1]);
-    painter.rotate(idw[1]); //旋转
-    painter.drawPixmap(0,0,width[1],height[1],pixfly);  //飞机2
-    painter.rotate(-idw[1]); //旋转
-    painter.translate(-idx[1],-idy[1]);
-
-    painter.translate(idx[2],idy[2]);
-    painter.rotate(idw[2]); //旋转
-    painter.drawPixmap(0,0,width[2],height[2],pixfly);  //飞机3
-    painter.rotate(-idw[2]); //旋转
-    painter.translate(-idx[2],-idy[2]);
+//    painter.translate(idx[2],idy[2]);
+//    painter.rotate(idw[2]); //旋转
+//    painter.drawPixmap(0,0,width[2],height[2],pixfly);  //飞机3
+//    painter.rotate(-idw[2]); //旋转
+//    painter.translate(-idx[2],-idy[2]);
 
     //painter.translate(-100, -100);  //让图片的中心作为旋转的中心
 
